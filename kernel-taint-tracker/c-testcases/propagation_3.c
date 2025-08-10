d0b5a73704a32f6a0766373e9f502386f6515312
Florin Zamfir <f.zamfir@student.tugraz.at>
2025-08-10 10:51:12 +0200

    restructured tests; added more testcases

---
+++ kernel-taint-tracker/c-testcases/propagation_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define INPUT_LENGTH 10
+
+void print_input(char* input_buffer) {
+  printf(input_buffer);        // sink
+}
+
+void pass_input(char* input_buffer) {
+  print_input(input_buffer);   // tainted because of param
+}
+
+int main(int argc, char* argv[]) {
+  char input_buffer[10];       // tainted after scanf
+  scanf("%s", input_buffer);   // tainted
+
+  pass_input(input_buffer);    // tainted because of param
+}
---
+++ kernel-taint-tracker/c-testcases/propagation_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define INPUT_LENGTH 10
+
+void print_input(char* input_buffer) {
+  printf(input_buffer);        // sink
+}
+
+void pass_input(char* input_buffer) {
+  print_input(input_buffer);   // tainted because of param
+
+  // tainted because it point to the 
+  // same tainted memory object
+  char* intermediate_buffer = input_buffer;
+
+  print_input(intermediate_buffer);
+}
+
+int main(int argc, char* argv[]) {
+  char input_buffer[10];       // tainted after scanf
+  scanf("%s", input_buffer);   // tainted
+
+  pass_input(input_buffer);    // tainted because of param
+}
